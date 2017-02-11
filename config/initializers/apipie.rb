18c40e6c095b6319284a857468ed9bddbac8d066
Jacob Bevilacqua <jacobbev@gmail.com>
2017-02-10 23:17:01 -0800

    Change path of docs

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,7 @@
 Apipie.configure do |config|
   config.app_name                = "Hack Merced 17"
   config.api_base_url            = "/api"
-  config.doc_base_url            = "/api"
+  config.doc_base_url            = "/docs"
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
   config.copyright               = "Study Space, Inc. 2017"
